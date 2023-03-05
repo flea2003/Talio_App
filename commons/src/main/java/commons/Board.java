f82236815a8e438a177a1023e800796894b1f94c
ANTREAS IOANNOU <a.ioannou@student.tudelft.nl>
2023-03-05 15:53:47 +0100

    Add classes for commons User,Board

---
+++ commons/src/main/java/commons/Board.java
@@ -0,0 +1,51 @@
+package commons;
+import static org.apache.commons.lang3.builder.ToStringStyle.MULTI_LINE_STYLE;
+
+import javax.persistence.*;
+
+import commons.List;
+import commons.Person;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
+import java.util.Map;
+
+@Entity
+public class Board {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public long id;
+
+    @OneToMany(cascade = CascadeType.PERSIST)
+    public Map<Long, List> lists;
+
+    public String name;
+
+
+    @SuppressWarnings("unused")
+    public Board() {}
+
+    public Board(long id, Map<Long, List> lists, String name) {
+        this.id = id;
+        this.lists = lists;
+        this.name = name;
+    }
+
+
+    @Override
+    public boolean equals(Object obj) {
+        return EqualsBuilder.reflectionEquals(this, obj);
+    }
+
+    @Override
+    public int hashCode() {
+        return HashCodeBuilder.reflectionHashCode(this);
+    }
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this, MULTI_LINE_STYLE);
+    }
+}
