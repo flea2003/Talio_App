35a4c21497a352a9b8c07e3b91b117eb8436b360
Rafael Alani <r.a.alani-1@student.tudelft.nl>
2023-03-08 19:13:43 +0100

    fixed checkstyle compliance

--- server/src/main/java/server/api/CardController.java
+++ server/src/main/java/server/api/CardController.java
@@ -6,7 +6,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import server.database.CardRepository;
 
-import java.util.Collections;
 import java.util.List;
 
 @RestController
--- server/src/main/java/server/database/CardRepository.java
+++ server/src/main/java/server/database/CardRepository.java
@@ -3,7 +3,6 @@ package server.database;
 import commons.Card;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import javax.swing.text.html.Option;
 import java.util.List;
 import java.util.Optional;
 
