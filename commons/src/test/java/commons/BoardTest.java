5ec48a16195011a9afef2d3908dae220c4968976
ANTREAS IOANNOU <a.ioannou@student.tudelft.nl>
2023-03-08 16:29:16 +0100

    Adjust commons

--- commons/src/test/java/commons/BoardTest.java
+++ commons/src/test/java/commons/BoardTest.java
@@ -3,8 +3,6 @@ package commons;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertTrue;
