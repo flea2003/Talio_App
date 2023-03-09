ee2ea5226255058214f48777bd0f28c8d6a7fec9
ANTREAS IOANNOU <a.ioannou@student.tudelft.nl>
2023-03-09 17:10:17 +0100

    Add TaskViewCtrl class

--- commons/src/main/java/commons/Card.java
+++ commons/src/main/java/commons/Card.java
@@ -50,4 +50,12 @@ public class Card {
     public String getDescription() {
         return description;
     }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
