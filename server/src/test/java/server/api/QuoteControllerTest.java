b557bbc0de674a9850de282741ed3742b12a6dd0
Victor Purice <V.Purice@student.tudelft.nl>
2023-03-16 23:38:17 +0100

    fixed the pipeline

--- server/src/test/java/server/api/QuoteControllerTest.java
+++ server/src/test/java/server/api/QuoteControllerTest.java
@@ -39,7 +39,7 @@ public class QuoteControllerTest {
     public void setup() {
         random = new MyRandom();
         repo = new TestQuoteRepository();
-        sut = new QuoteController(random, repo, null);
+        sut = new QuoteController(random, repo);
     }
 
     @Test
