e8391962ae03b9311afab3af0d7a39dd1ac7b2c4
ANTREAS IOANNOU <a.ioannou@student.tudelft.nl>
2023-03-28 14:53:46 +0200

    Add confirmation for deletion

--- client/src/main/java/client/scenes/DashboardCtrl.java
+++ client/src/main/java/client/scenes/DashboardCtrl.java
@@ -419,7 +419,7 @@ public class DashboardCtrl implements Initializable {
 
             // Delete List Button
             delete.setOnAction(e -> {
-                server.deleteList((Long) label.getUserData());
+                deleteList((Long)label.getUserData());
             });
 
             //Create a list
@@ -431,6 +431,25 @@ public class DashboardCtrl implements Initializable {
             hboxButtons.getChildren().add(edit);
 
             addCards(listCurr, vBox, listView);
+        }
+    }
+
+    private void deleteList(Long listId) {
+
+        List list = server.getList(listId);
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setHeaderText("Delete List '" + list.getName() + "'?");
+        alert.setContentText("Are you sure you want to delete list '" + list.getName() +
+                "'?\nThis will permanently delete the list from the server.");
+
+        ButtonType delete = new ButtonType("Delete");
+        ButtonType cancel = new ButtonType("Cancel");
+        alert.getButtonTypes().setAll(delete, cancel);
+
+        Optional<ButtonType> result = alert.showAndWait();
+
+        if (result.get() == delete) {
+            server.deleteList(listId);
         }
     }
 
@@ -709,9 +728,14 @@ public class DashboardCtrl implements Initializable {
                 } else {
                     retrievedBoard = server.getBoardByKey(key);
                     if(retrievedBoard != null ) {
-                        if (connectedBoards.contains(retrievedBoard)) {
-                            errorMessage.setText("Board is already added");
-                        } else {
+                        boolean boardAdded = false;
+                        for(Board board : connectedBoards){
+                            if(board.id == retrievedBoard.id){
+                                errorMessage.setText("Board is already added");
+                                boardAdded = true;
+                            }
+                        }
+                        if (!boardAdded){
                             errorMessage.setText("");
                             connectedBoards.add(retrievedBoard);
                             serverBoards.get(server.getSERVER()).add(retrievedBoard);
