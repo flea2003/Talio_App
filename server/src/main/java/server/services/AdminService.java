3247666712e16bf5c3bd45c105898d62c47b7e34
Rafael Alani <r.a.alani-1@student.tudelft.nl>
2023-04-11 00:07:44 +0200

    nice admin password prompt

--- server/src/main/java/server/services/AdminService.java
+++ server/src/main/java/server/services/AdminService.java
@@ -14,17 +14,26 @@ public class AdminService {
      */
     @PostConstruct
     private void initPassword(){
-        int leftLimit = 97; // letter 'a'
-        int rightLimit = 122; // letter 'z'
-        int targetStringLength = 10;
-        Random random = new Random();
+        String promptStart = "=========================================\n";
+        Thread thread = new Thread(() -> {
+            try {
+                Thread.sleep(1500);
+                int leftLimit = 97; // letter 'a'
+                int rightLimit = 122; // letter 'z'
+                int targetStringLength = 10;
+                Random random = new Random();
 
-        String generatedString = random.ints(leftLimit, rightLimit + 1)
-                .limit(targetStringLength)
-                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
-                .toString();
-        serverPassword = generatedString;
-        System.out.println(serverPassword);
+                String generatedString = random.ints(leftLimit, rightLimit + 1)
+                        .limit(targetStringLength)
+                        .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+                        .toString();
+                serverPassword = generatedString;
+                System.out.println(promptStart + "Server password: " + serverPassword + "\n" + promptStart);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+        thread.start();
     }
 
     /**
